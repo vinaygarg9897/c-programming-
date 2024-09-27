57970525d7b993dbc5468ea6505abf092ff06e64
vinaygarg9897 <gargv1790@gmail.com>
2024-09-27 11:56:36 +0530

    Add files via upload

---
+++ SIZEOFIN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+   printf("\nsize of int:%u",(sizeof(int)));
+   printf("\nsize of float:%u",(sizeof(float)));
+   printf("\nsize of char :%u",(sizeof(char)));
+   printf("\nsize of double:%u",(sizeof(double)));
+   getch();
+   }
+
