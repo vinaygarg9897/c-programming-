11efb897f83474a8780bec2f02b0a23cf03e3852
vinaygarg9897 <gargv1790@gmail.com>
2024-10-04 12:25:11 +0530

    Add files via upload

---
+++ INTEGER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m;
+clrscr();
+printf("enter integer");
+scanf("%d",&m);
+if(m>0)
+printf("n=1");
+else if(m==0)
+printf("n=0");
+else
+printf("n=-1");
+getch();
+}
