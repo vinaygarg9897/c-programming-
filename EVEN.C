1dcef294921cf0ff04b4809611375defcefa9543
vinaygarg9897 <gargv1790@gmail.com>
2024-10-06 16:06:49 +0530

    Add files via upload

---
+++ EVEN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the no");
+scanf("%d",&num);
+if(num%2==0)
+printf("no is even");
+else
+printf("no is odd");
+getch();
+}
