8dc33b6dad10d894449726f4860567aac1477a0e
vinaygarg9897 <gargv1790@gmail.com>
2024-10-08 15:50:05 +0530

    Add files via upload

---
+++ TABLE.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+clrscr();
+printf("enter no");
+scanf("%d",&n);
+for(i=1;i<=10;i++)
+{
+printf("\n%d*%d= %d",n,i,n*i);
+}
+getch();
+}
+
+
+
+
+
+
+
+
