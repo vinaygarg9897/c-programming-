5acc0120cea89703d2f63d650fde29be1b754bf9
vinaygarg9897 <gargv1790@gmail.com>
2024-10-08 15:28:44 +0530

    Add files via upload

---
+++ SUMODD.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0,i;
+clrscr();
+printf("enter no");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(i%2!=0)
+{
+sum=sum+i;
+printf("\n%d",i);
+}
+}
+printf("\n%d",sum);
+getch();
+}
+
+
+
+
+
+
+
+
+
