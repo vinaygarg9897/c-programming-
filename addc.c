bffeb8667293485dfaa4f62bf276fcabb203f15d
vinaygarg9897 <gargv1790@gmail.com>
2024-11-06 14:14:28 +0530

    Add files via upload

---
+++ addc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void add(int,int);
+
+int main()
+{
+	int a,b;
+	printf("enter two no");
+	scanf("%d%d",&a,&b);
+	add(a,b);
+	
+	return 0;
+}
+	
+	void add(int a,int b)
+	{
+		int c;
+		c=a+b;
+	printf("sum is =%d",c);
+	}
+	
+
